742941064a898606a43503e35bce89165efa0cd2
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-04-02 19:48:51 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,12 @@
+function checkEqual(a,b)
+{
+    return a===b ? true : false;
+
+}
+console.log(checkEqual(6,6));
+
+function checkSign(num)
+{
+    return num>0 ? "positive" : num<0 ? "negative" : "zero";
+}
+console.log(checkSign(0));
